5c3478437b37633bac4562fb7ad7664b9fa13ec3
jgy2808 <jgy2808@naver.com>
2021-07-19 20:25:03 +0900

    Add client thread class
    Will implement receive message from server thread.

---
+++ ClientMain/src/application/ClientThread.java
@@ -0,0 +1,15 @@
+package application;
+
+public class ClientThread extends Thread {
+	Main mc;
+	
+	public ClientThread(Main mc) {
+		this.mc = mc;
+	}
+	
+	// 서버로부터 메세지 받아서 처리하고 화면에 출력하기
+	public void run() {
+		
+	}
+	
+}
