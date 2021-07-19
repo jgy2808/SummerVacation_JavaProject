250e4e1d08b6e4bcdfafb1f3686cbf5455883ff1
jgy2808 <jgy2808@naver.com>
2021-07-19 20:21:05 +0900

    Don't use this file

---
+++ ClientThread/src/clientThread/ClientThread.java
@@ -0,0 +1,15 @@
+package clientThread;
+
+import application.Main;
+
+public class ClientThread extends Thread{
+	Main mc;
+	
+	public ClientThread(Main mc) {
+		this.mc = mc;
+	}
+	
+	public void run() {
+		
+	}
+}
