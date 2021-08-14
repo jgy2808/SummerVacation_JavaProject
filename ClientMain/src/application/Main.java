6139e5dff39dd951c1a181fcb85a58f5ae1191ad
jgy2808 <jgy2808@naver.com>
2021-08-15 02:53:21 +0900

    Update Main.java

--- ClientMain/src/application/Main.java
+++ ClientMain/src/application/Main.java
@@ -14,9 +14,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	Socket socketroominfo;
 
-//	FXMLLoader f;
-//	Parent r;
-//	Stage stage2;
+	FXMLLoader f;
+	Parent r;
+	Stage stage2;
 	
 	public void openWaitingroom() {
 		Thread thread = new Thread() {
@@ -48,31 +48,32 @@ public class Main extends Application {
 		//openWaitingroom();
 		
 		Parent root = FXMLLoader.load(getClass().getResource("view/main1.fxml"));
-		
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setOnCloseRequest(event -> closeWindow());
-		primaryStage.setScene(new Scene(root));
+		primaryStage.setScene(scene);
 		primaryStage.setTitle("YSB");
 		primaryStage.show();
 	}
 	
-//	public void chattingScene(Stage stage) {
-//		try {
-//			f = new FXMLLoader(getClass().getResource("main1.fxml"));
-//			
-//			r = (Parent) f.load();
-//			stage2 = new Stage();
-//			stage2.setScene(new Scene(r));
-//			stage2.setTitle("YSB2");
-//			stage2.show();				// 새로운 창을 여는 코드	
-//			
-////			Stage tmp = (Stage) btn.getScene().getWindow();
-////			tmp.close();		// 해당 두줄은 방입장이 기존 대기실방 닫는 코드
-//		}
-//		catch(IOException ex) {
-//			System.out.println(ex);
-//			ex.printStackTrace();
-//		}
-//	}
+	public void chattingScene(Stage stage) {
+		try {
+			f = new FXMLLoader(getClass().getResource("view/main2.fxml"));
+			
+			r = (Parent) f.load();
+			stage2 = new Stage();
+			stage2.setScene(new Scene(r));
+			stage2.setTitle("YSB2");
+			stage2.show();				// 새로운 창을 여는 코드	
+			
+//			Stage tmp = (Stage) btn.getScene().getWindow();
+//			tmp.close();		// 해당 두줄은 방입장이 기존 대기실방 닫는 코드
+		}
+		catch(IOException ex) {
+			System.out.println(ex);
+			ex.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 		launch(args);
