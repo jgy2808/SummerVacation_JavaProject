7b0bd316ac8716d26f3412862e7189ee7972b160
jgy2808 <jgy2808@naver.com>
2021-08-23 21:03:33 +0900

    Update Main.java

--- ClientMain/src/application/Main.java
+++ ClientMain/src/application/Main.java
@@ -1,9 +1,6 @@
 package application;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
 
 import application.view.Controller2;
 import application.view.Controller3;
@@ -15,10 +12,6 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	Controller2 c2 = new Controller2();
-	Controller3 c3 = new Controller3();
-	FXMLLoader f;
-	Parent r;
-	Stage stage2;
 	
 	public void start(Stage primaryStage) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("view/main1.fxml"));
@@ -28,26 +21,6 @@ public class Main extends Application {
 		primaryStage.setTitle("YSB");
 		primaryStage.setOnCloseRequest(event -> c2.closeWaitingRoom());
 		primaryStage.show();
-	}
-	
-	public void chattingScene(String title) {
-		try {
-			f = new FXMLLoader(getClass().getResource("view/main2.fxml"));
-			
-			r = (Parent) f.load();
-			stage2 = new Stage();
-			stage2.setScene(new Scene(r));
-			stage2.setTitle(title);
-			stage2.setOnCloseRequest(event -> c3.closeChattingRoom());
-			stage2.show();				// 새로운 창을 여는 코드	
-			
-//			Stage tmp = (Stage) btn.getScene().getWindow();
-//			tmp.close();		// 해당 두줄은 방입장이 기존 대기실방 닫는 코드
-		}
-		catch(IOException ex) {
-			System.out.println(ex);
-			ex.printStackTrace();
-		}
 	}
 
 	public static void main(String[] args) {
