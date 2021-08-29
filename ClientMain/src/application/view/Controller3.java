edfeb10a881431e9db85028ee84d003a87b70249
jgy2808 <jgy2808@naver.com>
2021-08-29 16:16:05 +0900

    Update Controller3.java

--- ClientMain/src/application/view/Controller3.java
+++ ClientMain/src/application/view/Controller3.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.ResourceBundle;
 
 import application.db.DBConnect;
